b875f4195cb3bec90b67e688072521cb6013707c
Matthew Collins <matthew.collins1991@yahoo.com>
2019-01-16 15:27:22 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -13,7 +13,10 @@ end
 
 
 def select_value_and_count_of_most_prolific_species
-  "SELECT value FROM characters WHERE species"
+  "SELECT species, COUNT(species) AS species_count
+  FROM characters
+  GROUP BY species
+  ORDER BY species_count DESC LIMIT 1"
 end
 
 def select_name_and_series_subgenres_of_authors
