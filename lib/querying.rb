51c1c7a62f8dfad8498599f98b4b5101f343cbbc
Matthew Collins <matthew.collins1991@yahoo.com>
2019-01-16 15:25:46 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,14 +1,19 @@
 def select_books_titles_and_years_in_first_series_order_by_year
-  "Write your SQL query here"
+  "SELECT DISTINCT title, year 
+  FROM books 
+  WHERE series_id = 1
+  ORDER BY year"
 end
 
 def select_name_and_motto_of_char_with_longest_motto
-  "Write your SQL query here"
+  "SELECT name, motto
+  FROM characters
+  ORDER BY LENGTH(motto) DESC LIMIT 1"
 end
 
 
 def select_value_and_count_of_most_prolific_species
-  "Write your SQL query here"
+  "SELECT value FROM characters WHERE species"
 end
 
 def select_name_and_series_subgenres_of_authors
