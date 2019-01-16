3ef6b5ca1cce6736c5ad8f4fe02d388d45116c86
Matthew Collins <matthew.collins1991@yahoo.com>
2019-01-16 15:28:19 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -29,7 +29,13 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "Write your SQL query here"
+  "SELECT series.title
+  FROM series
+  INNER JOIN characters
+  ON series.id = characters.series_id
+  WHERE characters.species = 'human'
+  GROUP BY series.title
+  ORDER BY COUNT(characters.species = 'human') DESC LIMIT 1"
 end
 
 def select_character_names_and_number_of_books_they_are_in
